79fc2c804b5d123a8029c751c6cf9cc00938ebd3
Akash-Builds <pattnaikakash27@gmail.com>
2024-12-26 23:14:13 +0530

    Fix: Ensure UpdateBook.jsx is correctly imported

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -14,7 +14,7 @@ import DashboardLayout from "../pages/dashboard/DashboardLayout";
 import Dashboard from "../pages/dashboard/Dashboard";
 import ManageBooks from "../pages/dashboard/manageBooks/ManageBooks";
 import AddBook from "../pages/dashboard/addBook/AddBook";
-import UpdateBook from "../pages/dashboard/editbook/UpdateBook";
+import UpdateBook from "../pages/dashboard/EditBook/UpdateBook";
 import CheckOutPage from "../pages/books/CheckoutPage";
 
 
